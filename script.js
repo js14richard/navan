9d68430789f8df37cb299cdf0dc39aedac0d7261
js14richard <js14richard@gmail.com>
2024-06-09 20:23:59 +0530

    close-open chat button

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
 
-/*
-    Close the chat label when close button is clicked
-*/
-
-var closeChat = () => {
-    console.log("close chat callled")
-    var chatWidthLabelIframe = document.getElementsByClassName('chat-widget-label-iframe')[0];
-    chatWidthLabelIframe.style.display = 'none';
-}
 
 function sendMessageToParent(eventName) {
-    console.log("sendmessage called", eventName);
     window.parent.postMessage(eventName, '*');
 }
 
 window.addEventListener('message', function(event) {
-    console.log("received a message", event);
-    if (event.origin && event.origin !== window.location.origin) {
-        console.log("Event origin:", event.origin);
-        console.log("Window origin:", window.location.origin);
-        console.log("origin check");
+
+    /* Ensures that request is comming from the same origin */
+    /* Disable this condition if testing in local  */
+
+    if (event.origin !== window.location.origin) {
+        console.error("Post message is received from different origin")
         return;
     }
 
     if (event.data === 'closeChat') {
-        console.log("request for closing chat");
         closeChat();
     }
+
+    if (event.data === 'openChat') {
+        openChat();
+    }
+    
 });
+
+
+
+var closeChat = () => {
+    var chatWidthLabelIframe = document.getElementsByClassName('chat-widget-label-iframe')[0];
+    chatWidthLabelIframe.style.display = 'none';
+}
+
+var openChat = () => {
+    var chatWidthLabelIframe = document.getElementsByClassName('chat-widget-label-iframe')[0];
+    chatWidthLabelIframe.style.display = 'block';
+}
+
+
+
 
